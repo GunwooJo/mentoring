12ca6c6faf71bb38f2da0d5e8125ea1cdd2720d7
GunwooJo <kanggi1997@naver.com>
2023-12-21 13:53:41 +0900

    feat: 상단바 초기셋팅

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Button from '@mui/material/Button';
-
+import Header from './components/Header';
 function App() {
     const [hello, setHello] = useState('')
 
@@ -13,6 +13,7 @@ function App() {
 
     return (
         <div>
+            <Header/>
             <div>
                 백엔드에서 가져온 데이터입니다 : {hello}
             </div>
